4a1b6ac0579a1b61ed37de2acc4f31ccb3e5f762
akshdesai1 <desaiaksh82@gmail.com>
2023-06-01 20:50:27 +0530

    Added Events page and Contact Us Page

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Stack } from "@mui/material";
 // import Review 2 from './Review 2';
-import Review from "./Review 2";
+import Review from "./Review";
 import '../styles/Event.css';
 import Jumbotron from "./Jumbotron";
 import Content from "./Content";
---
+++ src/components/contactPage/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContactJumbotron from './ContactJumbotron'
+import ContactContent from './ContactContent'
+
+const Contact = () => {
+  return (
+    <div>
+      <ContactJumbotron />
+      <ContactContent />
+    </div>
+  )
+}
+
+export default Contact
