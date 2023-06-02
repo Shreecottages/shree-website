805889a380c4ad95266f6b4f5c3a7243e2eab749
Krish Kanani <krishkanani148@gmail.com>
2023-06-02 10:27:20 +0530

    Added Image Gallery and About Us Pages

---
+++ src/App.jsx
@@ -0,0 +1,306 @@
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Homepage from "./components/Hompage";
+import CSlider from "./components/CSlider";
+import Review from "./components/Review";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import Ecard from "./components/Ecard";
+import Event from "./components/Event";
+import Experience from "./components/Experience";
+import Contact from "./components/contactPage/Contact";
+import AboutUsMain from "./components/AboutUsMain";
+
+import ImageGallery from "./components/FarmImageGallery/ImageGallery";
+import ImageGalleryView from "./components/FarmImageGallery/ImageGalleryView";
+import VideoGalleryView from "./components/FarmImageGallery/VideoGalleryView";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FFE2D8",
+    },
+    secondary: {
+      light: "#9E837D",
+      main: "#3D2521",
+    },
+    warning: {
+      main: "#FFBB70",
+      dark: "#FF867C",
+    },
+  },
+  typography: {
+    fontFamily: ["Bona Nova", "serif", "Inter", "Poppins"].join(","),
+    color: "#3D2521",
+    subtitle1: {
+      color: "#FFFFFF",
+      fontFamily: "Poppins",
+      fontSize: "1vw",
+      "@media (max-width:800px)": {
+        fontSize: "1.4vw",
+      },
+    },
+    subtitle2: {
+      fontFamily: "Inter",
+      fontSize: "0.8vw",
+      "@media (max-width:900px)": {
+        fontSize: "1.2vw",
+      },
+    },
+    h2: {
+      fontSize: "2.25rem",
+      "@media (max-width:600px)": {
+        fontSize: "2rem",
+      },
+      color: "#3D2521",
+      fontWeight: "400",
+    },
+    h3: {
+      fontSize: "2vw",
+      fontWeight: "400",
+    },
+    h4: {
+      fontSize: "1.75rem",
+      "@media (max-width:600px)": {
+        fontSize: "1.5rem",
+      },
+      fontWeight: "400",
+      // wordWrap: 'normal',
+      // flexWrap: 'nowrap'
+    },
+    h5: {
+      fontSize: "1.35rem",
+      "@media (max-width:600px)": {
+        fontSize: "1.25rem",
+      },
+      fontWeight: "400",
+    },
+    h6: {
+      fontSize: "1.3vw",
+    },
+    body1: {
+      fontFamily: "Poppins",
+      color: "#484848",
+      fontSize: "1.2rem",
+      "@media (max-width:600px)": {
+        fontSize: "1rem",
+      },
+      letterSpacing: "0.07em",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        contained: {
+          background: "linear-gradient(180deg, #FFAA7D 0%, #FFBB70 100%);",
+          color: "#3D2521",
+          border: "none",
+          boxShadow: "none",
+          fontFamily: "Bona Nova",
+          fontSize: "0.8rem",
+          "@media (min-width:600px)": {
+            fontSize: "1rem",
+          },
+          "@media (min-width:800px)": {
+            fontSize: "1.2rem",
+          },
+          textTransform: "none",
+        },
+        contained2: {
+          background: "#FFBB70",
+          color: "#3D2521",
+          boxShadow: "none",
+          fontFamily: "Bona Nova",
+          fontSize: "0.8rem",
+          "@media (min-width:600px)": {
+            fontSize: "1rem",
+          },
+          "@media (min-width:800px)": {
+            fontSize: "1.2rem",
+          },
+          textTransform: "none",
+          "&:hover": {
+            cursor: "pointer",
+            background: "transparent",
+            border: "2px solid #3D2521",
+            color: "#3D2521",
+            boxShadow: "none",
+            transition: "0.4s",
+          },
+        },
+        outlined: {
+          background: "none",
+          border: "2px solid #3D2521",
+          boxShadow: "none",
+          fontFamily: "Bona Nova",
+          color: "#3D2521",
+          fontSize: "0.8rem",
+          "@media (min-width:600px)": {
+            fontSize: "1rem",
+          },
+          "@media (min-width:800px)": {
+            fontSize: "1.2rem",
+          },
+          padding: "5px 20px",
+          "@media (max-width:700px)": {
+            padding: "2.5px 10px",
+          },
+          textTransform: "none",
+          background: "linear-gradient(to left, white 50%, #FFBB70 50%) right",
+          backgroundSize: "200%",
+          "&:hover": {
+            cursor: "pointer",
+            backgroundPosition: "left",
+            color: "#3D2521",
+            boxShadow: "none",
+            transition: "0.4s",
+          },
+        },
+      },
+    },
+    MuiDrawer: {
+      styleOverrides: {
+        paper: {
+          backgroundColor: "#FFF6F3",
+          width: "100%",
+          height: "100%",
+        },
+      },
+    },
+    MuiMobileStepper: {
+      styleOverrides: {
+        dotActive: {
+          width: "3rem",
+          hight: "0.5vw",
+          borderRadius: "1vw",
+          backgroundColor: "#3D2521",
+          transform: "scale(0.55)",
+          // marginRight: '-0.5rem',
+          // marginLeft: '-0.5rem',
+          padding: "0",
+        },
+        dot: {
+          backgroundColor: "#C9A29C",
+          width: "2rem",
+          hight: "0.5vw",
+          borderRadius: "1vw",
+          marginRight: "-0.75rem",
+          marginleft: "-0.75rem",
+          transform: "scale(0.5)",
+          padding: "0",
+        },
+      },
+    },
+    MuiMenu: {
+      styleOverrides: {
+        color: "#3D2521",
+      },
+    },
+    MuiPopover: {
+      styleOverrides: {
+        paper: {
+          backgroundColor: "transparent",
+          border: "none",
+          boxShadow: "none",
+          borderRadius: "0px",
+        },
+      },
+    },
+    MuiListItemText: {
+      styleOverrides: {
+        typography: {
+          color: "#3D2521",
+          fontFamily: "Bona Nova",
+          textTransform: "none",
+          opacity: 1,
+          fontSize: {
+            xs: "1rem",
+            sm: "1.2rem",
+            md: "1.4rem",
+          },
+          "&:hover": {
+            background: "#000000",
+          },
+        },
+      },
+    },
+    MuiMenuItem: {
+      styleOverrides: {
+        typography: {
+          fontFamily: "Bona Nova",
+        },
+      },
+    },
+    MuiButtonBase: {
+      styleOverrides: {
+        fontFamily: "Bona Nova",
+        tab: {
+          textTransform: "none",
+          opacity: "1",
+          fontSize: {
+            xs: "1rem",
+            sm: "1.2rem",
+            md: "1.4rem",
+          },
+        },
+      },
+    },
+    MuiCardHeader: {
+      styleOverrides: {
+        titleTypography: {
+          variant: "h2",
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        Appbar: {
+          color: "#3D2521",
+        },
+      },
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <Header />
+        <div className="body">
+          {/* <Homepage /> */}
+          {/* <AboutUs/> */}
+          {/* <Event /> */}
+          {/* <Contact /> */}
+          {/* <AboutUsMain /> */}
+          {/* <Experience /> */}
+
+          <Router>
+            <ImageGallery />
+
+            <Routes>
+              <Route
+                exact
+                path="/gallery/images/all"
+                element={<ImageGalleryView />}
+              />
+
+              <Route
+                exact
+                path="/gallery/videos"
+                element={<VideoGalleryView />}
+              />
+            </Routes>
+          </Router>
+        </div>
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/FarmImageGallery/ImageGallery.jsx
@@ -0,0 +1,227 @@
+import React from "react";
+import { Box, Stack, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
+// IMPORTING STYLES
+import "./ImageGallery.css";
+
+// IMPORTING IMAGE
+import AboutUsTop from "../../assets/AboutTopImg.png";
+import AboutFLowerRight from "../../assets/AboutFlowersRight.png";
+import AboutFlowerLeft from "../../assets/AboutFlowerLeft.png";
+
+const ImageGallery = () => {
+  return (
+    <div>
+      <Box
+        sx={{
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: { sm: "100%" },
+            height: { xs: "300px", lg: "300px", xl: "326px" },
+            backgroundImage: `url(${AboutUsTop})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundColor: "rgba(61, 37, 33, 0.5)",
+            boxShadow:
+              "inset 130px -130px 250px rgba(61, 37, 33, 0.9), inset -130px 130px 250px rgba(61, 37, 33, 0.9)",
+          }}
+        >
+          <Box
+            sx={{
+              marginLeft: { sx: "-30px", lg: "200px" },
+              backgroundImage: `url(${AboutFlowerLeft})`,
+              backgroundRepeat: "no-repeat",
+              width: { xs: "250px", md: "250px" },
+              marginTop: "160px",
+            }}
+          ></Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            <Typography
+              sx={{
+                padding: { xs: "60px 0 20px 0", md: "100px 0 10px 0" },
+                fontFamily: "Bona Nova",
+                fontSize: { xs: "2.5em", md: "2.5em" },
+                fontWeight: "400",
+                lineHeight: "48px",
+                letterSpacing: "0.07em",
+                color: "#FFBB70",
+              }}
+            >
+              Gallery
+            </Typography>
+            <Box
+              sx={{
+                margin: "0px auto",
+                height: "30px",
+                width: "0px",
+                border: "1px solid #FFBB70",
+              }}
+            ></Box>
+
+            <Box
+              sx={{
+                width: { xs: "90%", md: "60%" },
+                display: "flex",
+                justifyContent: "space-between",
+                gap: { xs: "40px", md: "200px" },
+              }}
+            >
+              <Link className="ResortPartsLink" to="/gallery/images/all">
+                <Typography
+                  sx={{
+                    fontFamily: "Bona Nova",
+                    color: "#FFFFFF",
+                    fontSize: "30px",
+                    "&:hover": {
+                      color: "#FFBB70",
+                    },
+                  }}
+                >
+                  Images
+                </Typography>
+              </Link>
+
+              <Link className="ResortPartsLink" to="/gallery/videos">
+                <Typography
+                  sx={{
+                    fontFamily: "Bona Nova",
+                    color: "#FFFFFF",
+                    fontSize: "30px",
+                    "&:hover": {
+                      color: "#FFBB70",
+                    },
+                  }}
+                >
+                  Videos
+                </Typography>
+              </Link>
+            </Box>
+          </Box>
+
+          <Box
+            sx={{
+              marginRight: { sx: "50px", lg: "200px" },
+              backgroundImage: `url(${AboutFLowerRight})`,
+              backgroundRepeat: "no-repeat",
+              width: { xs: "250px", md: "250px" },
+            }}
+          ></Box>
+
+          {/* <Box sx={{ marginRight: { sx: "50px", lg: "200px" } }}>
+            <img
+              src={AboutFLowerRight}
+              style={{ width: "250px", height: "164px" }}
+              alt=""
+            />
+          </Box> */}
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          width: "100%",
+          marginTop: "100px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            width: { xs: "80%", sm: "100%", md: "64%" },
+            textDecoration: "none",
+            gap: { sx: "20px", sm: "18px", md: "30px" },
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Link
+            className="ResortPartsLink"
+            to="/gallery/images/all"
+            sx={{
+              width: { xs: "100%", sm: "auto" },
+              marginBottom: "10px",
+              // fontSize: { xs: "14px", sm: "24px", md: "30px" },
+            }}
+          >
+            All
+          </Link>
+          <Link
+            className="ResortPartsLink"
+            to="/gallery/resort"
+            sx={{
+              width: { xs: "100%", sm: "auto" },
+              marginBottom: "10px",
+              // fontSize: "20px",
+            }}
+          >
+            Resort
+          </Link>
+          <Link
+            className="ResortPartsLink"
+            to="/gallery/decoration"
+            sx={{
+              width: { xs: "100%", sm: "auto" },
+              marginBottom: "10px",
+            }}
+          >
+            Decoration
+          </Link>
+          <Link
+            className="ResortPartsLink"
+            to="/gallery/banquethall"
+            sx={{
+              width: { xs: "100%", sm: "auto" },
+              marginBottom: "10px",
+            }}
+          >
+            Banquet Hall
+          </Link>
+          <Link
+            className="ResortPartsLink"
+            to="/gallery/conferencehall"
+            sx={{
+              width: { xs: "100%", sm: "auto" },
+              marginBottom: "10px",
+            }}
+          >
+            Conference Hall
+          </Link>
+          <Link
+            className="ResortPartsLink"
+            to="/gallery/swimmingpool"
+            sx={{
+              width: { xs: "100%", sm: "auto" },
+              marginBottom: "10px",
+            }}
+          >
+            Swimming Pool
+          </Link>
+        </Box>
+      </Box>
+
+      {/* {data.all.map((item, index) => (
+        <img className={index} src={item} key={index} alt="First slide" />
+      ))} */}
+
+      {/* <CarouselSlider /> */}
+    </div>
+  );
+};
+
+export default ImageGallery;
