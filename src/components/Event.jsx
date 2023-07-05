6801e8d4d04c9b5a565262f61f57dc7be38ed5ea
Dhairya Bhavsar <dhairyabhavsar@Dhairyas-MacBook-Air.local>
2023-07-05 19:38:10 +0530

    react helmet added

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -5,11 +5,23 @@ import Review from "./Review";
 import '../styles/Event.css';
 import Jumbotron from "./Jumbotron";
 import Content from "./Content";
+import { Helmet } from "react-helmet";
 
 const Event = () => {
 
     return (
         <Stack>
+            <Helmet>
+                <title>Events & Meetings - Shree Cottages</title>
+                <meta
+                    name="description"
+                    content="Different events held at Shree Cottages"
+                />
+                <meta 
+                    name="keywords"
+                    content="events,shree cottages, hotel, resorts, weddings, meetings"
+                />
+            </Helmet>
             <Box sx={{overflowX:"hidden"}}>
                 <Jumbotron />
                 <Content />
