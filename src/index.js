016487c09f22a7ba6c4be394f5a4ad621178b58b
shree innovation <shreeinnovation@gmail.com>
2023-07-06 11:36:11 +0530

    install react snap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
+import { hydrate, render } from "react-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +14,13 @@ root.render(
     </BrowserRouter>
   // </React.StrictMode>
 );
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<root />, rootElement);
+} else {
+  render(<root />, rootElement);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
