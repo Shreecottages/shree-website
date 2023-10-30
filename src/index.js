964a2bf595acbeb7de9689635b02c8e6b8bdf756
shree innovation <shreeinnovation@gmail.com>
2023-10-30 17:37:40 +0530

    build is not working problem sloved

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
-import { hydrate, render } from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
+ReactDOM.hydrate(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<root />, rootElement);
-} else {
-  render(<root />, rootElement);
-}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
